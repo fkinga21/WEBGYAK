8fec13e1a76458750cdcc15b83355db5b68050c9
Baricza Lili Viktória <157923923+lilibaricza@users.noreply.github.com>
2026-04-30 15:25:22 +0200

    regisztral.php

--- logicals/regisztral.php
+++ logicals/regisztral.php
@@ -1,9 +1,10 @@
 <?php
+// Ellenőrizzük, hogy minden szükséges POST adat megérkezett-e az űrlapról
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=mysql.omega;dbname=cukorweb1', 'cukorweb1', 'sutike21*',
-                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh = new PDO('mysql:host=mysql.omega;dbname=cukorweb1', 'cukorweb1', 'sutike21*', // adatbázis elérési út, felhasználónév, jelszó 
+                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)); // hibakezelés: kivétel dobása
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
@@ -20,24 +21,30 @@ if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['veze
                           values(0, :csaladinev, :utonev, :bejelentkezes, :jelszo)";
             $stmt = $dbh->prepare($sqlInsert); 
             $stmt->execute(array(':csaladinev' => $_POST['vezeteknev'], ':utonev' => $_POST['utonev'],
-                                 ':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => sha1($_POST['jelszo']))); 
+                                 ':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => sha1($_POST['jelszo'])));
+            // Ellenőrizzük, hogy történt-e beszúrás
             if($count = $stmt->rowCount()) {
+                // Lekérjük az újonnan létrehozott rekord ID-ját
                 $newid = $dbh->lastInsertId();
+                // Sikeres regisztráció üzenet
                 $uzenet = "A regisztrációja sikeres.<br>Azonosítója: {$newid}.<br>Kérjük, <a href='belepes'>jelentkezzen be</a>.";
                 $ujra = false;
             }
             else {
+                 // Ha nem sikerült a beszúrás
                 $uzenet = "A regisztráció nem sikerült.";
                 $ujra = true;
             }
         }
     }
     catch (PDOException $e) {
+        // Adatbázis hiba esetén ide jutunk
         $uzenet = "Hiba: ".$e->getMessage();
         $ujra = true;
     }      
 }
 else {
+    // Ha nem érkeztek meg a szükséges adatok, visszairányítjuk a főoldalra
     header("Location: .");
 }
 ?>
