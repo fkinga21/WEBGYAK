d4a1758d5c2e54e498e5fe80c560373ea35471fb
fkinga21 <kinga041121@gmail.com>
2026-04-30 16:17:33 +0200

    navbar korrekcio

--- templates/pages/crud.tpl.php
+++ templates/pages/crud.tpl.php
@@ -21,24 +21,29 @@
             <?php if (isset($_SESSION['szerkeszt_id']) && $_SESSION['szerkeszt_id'] == $row['id']): ?>
                 <!-- SZERKESZTŐ MÓD: Input mezők jelennek meg -->
                 <input type="hidden" name="mentes_id" value="<?= $row['id'] ?>">
-                <td><input type="text" name="nev" value="<?= htmlspecialchars($row['nev']) ?>"></td>
-                <td><input type="text" name="tipus" value="<?= htmlspecialchars($row['tipus']) ?>"></td>
-                <td><input type="number" name="dijazott" value="<?= $row['dijazott'] ?>"></td>
-                <td><input type="text" name="mentes" value="<?= htmlspecialchars($row['mentes']) ?>"></td>
-                <td><input type="number" name="ertek" value="<?= $row['ertek'] ?>"></td>
-                <td><input type="text" name="egyseg" value="<?= htmlspecialchars($row['egyseg']) ?>"></td>
+                <td data-label="Név:"><input type="text" name="nev" value="<?= htmlspecialchars($row['nev']) ?>"></td>
+                <td data-label="Típus:"><input type="text" name="tipus" value="<?= htmlspecialchars($row['tipus']) ?>"></td>
+                <td data-label="Díjazott:">
+                    <select name="dijazott">
+                    <option value="1" <?= $row['dijazott'] == 1 ? 'selected' : '' ?>>Igen</option>
+                    <option value="0" <?= $row['dijazott'] == 0 ? 'selected' : '' ?>>Nem</option> 
+                    </select>   
+                </td>
+                <td data-label="Mentes:"><input type="text" name="mentes" value="<?= !empty($row['mentes']) ? htmlspecialchars($row['mentes']) : '&nbsp;' ?>"></td>
+                <td data-label="Ár:"><input type="number" name="ertek" value="<?= $row['ertek'] ?>"></td>
+                <td data-label="Egység:"><input type="text" name="egyseg" value="<?= htmlspecialchars($row['egyseg']) ?>"></td>
                 <td>
                     <button type="submit" class="btn-save">Mentés</button>
                     <button type="submit" name="megse" value="1" class="btn-exit">Mégse</button>
                 </td>
             <?php else: ?>
                 <!-- OLVASÓ MÓD: Sima szöveg és POST-os gombok -->
-                <td><?= htmlspecialchars($row['nev']) ?></td>
-                <td><?= htmlspecialchars($row['tipus']) ?></td>
-                <td><?= $row['dijazott'] ? 'Igen' : 'Nem' ?></td>
-                <td><?= htmlspecialchars($row['mentes']) ?></td>
-                <td><?= htmlspecialchars($row['ertek']) ?></td>
-                <td><?= htmlspecialchars($row['egyseg']) ?></td>
+                <td data-label="Név:"><?= htmlspecialchars($row['nev']) ?></td>
+                <td data-label="Típus:"><?= htmlspecialchars($row['tipus']) ?></td>
+                <td data-label="Díjazott:"><?= $row['dijazott'] ? 'Igen' : 'Nem' ?></td>
+                <td data-label="Mentes:"><?= !empty($row['mentes']) ? htmlspecialchars($row['mentes']) : '&nbsp;' ?></td>
+                <td data-label="Ár:"><?= htmlspecialchars($row['ertek']) ?></td>
+                <td data-label="Egység:"><?= htmlspecialchars($row['egyseg']) ?></td>
                 <td>
                     <!-- Edit gomb POST-al -->
                     <button type="submit" name="szerkeszt_id" value="<?= $row['id'] ?>" class="btn-edit">Edit</button>
